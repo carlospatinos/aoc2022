ccd642e9ebd69b368cedbb7020b7adff39767d2d
Carlos Patino <cpatinosoria@shutterstock.com>
2022-12-02 17:50:13 +0000

    c2p2 - 11657

--- c1/src/RockPaperScissors.kt
+++ c1/src/RockPaperScissors.kt
@@ -4,6 +4,8 @@ import java.io.File
 // Other player: rock = A; paper = B; scissors = C;
 val valueOfMyShape = mapOf<String, Int>("X" to 1, "Y" to 2, "Z" to 3)
 
+val whatShouldIdo = mapOf<String, Int>("X" to 0, "Y" to 3, "Z" to 6)
+
 // Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
 // If both players choose the same shape, the round instead ends in a draw.
 
@@ -13,12 +15,16 @@ val scoreOfGame = mapOf("A Y" to 6, "B Z" to 6, "C X" to 6,
                         "A Z" to 0, "B X" to 0, "C Y" to 0)
 
 
+val matchGuide = mapOf("A X" to "A Z", "B X" to "B X", "C X" to "C Y", // Lose
+    "A Y" to "A X", "B Y" to "B Y", "C Y" to "C Z", // Draw
+    "A Z" to "A Y", "B Z" to "B Z", "C Z" to "C X") // Win
+
 
 fun main(){
     var points = 0;
     File("./c2input.txt").forEachLine {
-        points += valueOfMyShape.getValue(it.last().toString())
-        points += scoreOfGame.getValue(it.toString())
+        points += valueOfMyShape.getValue(matchGuide.getValue(it.toString()).last().toString())
+        points += whatShouldIdo.getValue(it.last().toString())
     }
     println(points)
 }
