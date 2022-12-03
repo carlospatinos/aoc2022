dde6ba6475677eddf994b9392f1affe115d26c09
Carlos Patino <cpatinosoria@shutterstock.com>
2022-12-03 08:40:08 +0000

    working on part 2 of challenge 3

--- src/C3P2Rucksack.kt
+++ src/C3P2Rucksack.kt
@@ -1,27 +1,47 @@
 import java.io.File
 
+//fun mapChars(str: String) {
+
+//}
 fun main() {
     var total = 0;
     // a through z have priorities 1 through 26
     // A through Z have priorities 27 through 52
     val priorityValueBasedOnCharPosition = ('a'..'z') + ('A'..'Z')
 
+    var mapOfCharsAcross3Lines = mutableMapOf<Char, Double>()
+    var lineNumber :Int = 0
+    var group = mutableListOf<String>()
     File("./c3input.txt").forEachLine {
-        var mapOfChars = mutableMapOf<Char, Int>()
-        val mid = it.length / 2;
 
-        for (i in it.indices) {
-            val currentChar = it[i];
-            if (i < mid) {
-                mapOfChars.put(currentChar, 1);
-            } else {
-                if (mapOfChars.get(currentChar) == 1) {
+
+        if (lineNumber != 0 && lineNumber % 3 == 0) {
+
+            for (elfItems in group) {
+                for (i in elfItems.indices) {
+                    val currentChar = elfItems[i];
+                    var existingValue = mapOfCharsAcross3Lines.get(currentChar)
+                    if (existingValue != null) {
+                        mapOfCharsAcross3Lines.put(currentChar, existingValue)
+                    } else {
+                        mapOfCharsAcross3Lines.put(currentChar, Math.pow(1.0, 1.0*i))
+                    }
+
                     total += priorityValueBasedOnCharPosition.indexOf(currentChar) + 1
                     break;
                 }
             }
 
+            println("--------- new group")
+            group.clear()
         }
+        group.add(it)
+
+        lineNumber++;
+
+
     }
     println(total)
+
+
 }
